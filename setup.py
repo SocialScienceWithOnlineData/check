24bd1f97002198acb0b77db0ff3a7a0b9734159d
Seth Frey <moctodliamg@gmail.com>
2019-12-29 23:53:07 -0800

    continue tweaks

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='lessoncheck',
-    version='0.5.1',
-    url='https://github.com/enfascination/check.git',
+    version='0.5.2',
+    url='https://github.com/enfascination/lessoncheck.git',
     author='Seth Frey',
     author_email='sfrey@ucdavis.edu',
     description='Interactive low-stakes local micro-assessments for notebook based Python pedagogy, for UC Davis CMN152V',
