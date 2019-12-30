47e7029f0207d79f3ba08f245201f801bc72c941
Seth Frey <moctodliamg@gmail.com>
2019-12-29 16:36:59 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+### cribbed from https://github.com/maet3608/minimal-setup-py/blob/master/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='check',
+    version='0.5.0',
+    url='https://github.com/enfascination/check.git',
+    author='Seth Frey',
+    author_email='sfrey@ucdavis.edu',
+    description='Interactive low-stakes local micro-assessments for notebook based Python pedagogy, for UC Davis CMN152V',
+    packages=find_packages(),
+    install_requires=[],
+)
