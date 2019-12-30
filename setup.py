4d691b2df2e59d49c2c0e356b91a6f25bc6b4d66
Seth Frey <moctodliamg@gmail.com>
2019-12-29 23:32:51 -0800

    more tweaks to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 ### cribbed from https://github.com/maet3608/minimal-setup-py/blob/master/setup.py
 from setuptools import setup, find_packages
 
@@ -9,6 +10,5 @@ setup(
     author_email='sfrey@ucdavis.edu',
     description='Interactive low-stakes local micro-assessments for notebook based Python pedagogy, for UC Davis CMN152V',
     packages=find_packages(),
-    install_requires=[],
     license='MIT',
 )
